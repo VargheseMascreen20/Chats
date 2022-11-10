5877476a018a23fe7ea1054d6c3017c03ccfbd82
Varghese T Mascreen <varghese.t.mascreen20@gmail.com>
2022-11-10 15:14:00 +0530

    Initial Commit

---
+++ Chats/ChatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatsApp.swift
+//  Chats
+//
+//  Created by DDUKK7 on 10/11/22.
+//
+
+import SwiftUI
+
+@main
+struct ChatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
