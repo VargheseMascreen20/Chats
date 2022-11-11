1f49a6b08f33696d49cf0dc1bca2863ebc77fa7b
Varghese T Mascreen <varghese.t.mascreen20@gmail.com>
2022-11-11 15:22:51 +0530

    "Initial Commit"

--- Chats/ContentView.swift
+++ Chats/ContentView.swift
@@ -8,9 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isLoginMode = false
+    @State var email = ""
+    @State var password = ""
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            ScrollView{
+                VStack{
+                    Picker(selection: $isLoginMode,
+                           label: Text("Picker here")){
+                        Text("Login").tag(true)
+                        Text("Create Account").tag(false)
+                    }.pickerStyle(SegmentedPickerStyle())
+                    Button{}
+                label:{
+                    Image(systemName: "person.fill").font(.system(size:64)).padding()
+                }
+                    TextField("Email", text: $email)
+                    TextField("Password", text: $password)
+                    Button{} label: {
+                        HStack{
+                            Spacer()
+                            Text("Create Account").foregroundColor(Color.white).padding(.vertical,16)
+                            Spacer()
+                        }.background(Color.blue)
+                    }
+                    Text("Creation Account Page")
+                }.padding()
+                
+            }.navigationTitle("Create Account")
+        }
     }
 }
 
